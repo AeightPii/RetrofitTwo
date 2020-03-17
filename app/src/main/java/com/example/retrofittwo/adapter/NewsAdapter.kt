4275b6dcd4fa96b6e5821057fab78a83953d88a9
AeightPii <minhanacc@gmail.com>
2020-03-17 23:54:45 +0630

    change

--- app/src/main/java/com/example/retrofittwo/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/retrofittwo/adapter/NewsAdapter.kt
@@ -1,21 +1,30 @@
 package com.example.retrofittwo.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.retrofittwo.R
 import com.example.retrofittwo.model.Article
 import com.example.retrofittwo.model.News
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_news.view.*
 
 class NewsAdapter(var newsArray: ArrayList<Article>) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
     inner class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+       //var img:ImageView=itemView.findViewById(R.id.img_news)
         fun bind(news: Article) {
             itemView.txt_title.text = news.title
             itemView.txt_content.text = news.content
+            itemView.txt_author.text = news.author
+            Glide.with(itemView).load(news.urlToImage).into(itemView.img_news)
+           //Picasso.get().load(news.urlToImage).into(itemView.img_news)
+
         }
     }
 
